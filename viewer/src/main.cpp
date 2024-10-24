0449e3715b8d4524128d83c1a28b1d41c062964f
DjoserKhemSimeu <djoser.simeu@gmail.com>
2024-10-24 17:56:51 +0200

    correcting the surface self bounding problem

--- viewer/src/main.cpp
+++ viewer/src/main.cpp
@@ -93,7 +93,7 @@ int main( int argc, char* argv[] )
 {
     setlocale(LC_ALL,"C");
     QApplication app(argc, argv);
-    QString sceneName = "teapot.ply";
+    QString sceneName = "dragon.ply";
     QString textureName = "bricks.png";
     QString envMapName = "pisa.png";
 
